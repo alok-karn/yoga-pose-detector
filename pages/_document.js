68d7ae7391a003dfcf2af9de994de9c25ce29c98
alok-karn <alokkyasth@gmail.com>
2024-04-03 18:35:56 +0530

    fix: issue related with script

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                {/* <!-- Scripts for TensorFlow.js and Teachable Machine Image --> */}
-                <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js"></script>
-                <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@latest/dist/teachablemachine-image.min.js"></script>
+                
             </Head>
             <body>
                 <Main />
